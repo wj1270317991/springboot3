d7f64d982210d6fccdcc6a49a3566922ee72ce6f
wangjun <wangjun@sinoprof.com>
2024-02-07 10:34:27 +0800

    Springboot实现对配置文件中的明文密码加密 https://blog.csdn.net/m0_64355285/article/details/131828443

--- src/test/java/com/example17/demo17/Demo17ApplicationTests.java
+++ src/test/java/com/example17/demo17/Demo17ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example17.demo17;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentPBEConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Demo17ApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testEncrypt() throws Exception {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        EnvironmentPBEConfig config = new EnvironmentPBEConfig();
+
+        config.setAlgorithm("PBEWithMD5AndDES");          // 加密的算法，这个算法是默认的
+        config.setPassword("secrect");                        // 加密的密钥，随便自己填写，很重要千万不要告诉别人
+        standardPBEStringEncryptor.setConfig(config);
+        String plainText = "sino";         //自己的密码
+        String encryptedText = standardPBEStringEncryptor.encrypt(plainText);
+        System.out.println("密码：" + encryptedText);
     }
+
+    @Test
+    public void testDe() throws Exception {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        EnvironmentPBEConfig config = new EnvironmentPBEConfig();
+
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setPassword("secrect");
+        standardPBEStringEncryptor.setConfig(config);
+        String encryptedText = "oOuQJhZQF9JcHz0dba78cw==";   //加密后的密码
+        String plainText = standardPBEStringEncryptor.decrypt(encryptedText);
+        System.out.println(plainText);
+    }
+
+
 
 }
