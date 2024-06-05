adce1037964204f3924b17c5e103ad458b485275
wangjun <wangjun@sinoprof.com>
2024-06-05 17:19:22 +0800

    minio 文件下载上传

---
+++ src/main/java/com/example17/demo17/config/MinioConfig.java
@@ -0,0 +1,51 @@
+package com.example17.demo17.config;
+
+import io.minio.MinioClient;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * com.example17.demo17.config
+ * ClassName: MinioConfig
+ * Description:
+ * Create by: wangjun
+ * Date: 2024/6/4 15:07
+ */
+@Data
+@Configuration
+public class MinioConfig {
+    /**
+     * 访问地址
+     */
+    @Value("${minio.endpoint}")
+    private String endpoint;
+
+    /**
+     * accessKey类似于用户ID，用于唯一标识你的账户
+     */
+    @Value("${minio.accessKey}")
+    private String accessKey;
+
+    /**
+     * secretKey是你账户的密码
+     */
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    /**
+     * 默认存储桶
+     */
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
+    @Bean
+    public MinioClient minioClient() {
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+        return minioClient;
+    }
+}
